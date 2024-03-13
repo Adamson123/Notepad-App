3b079024ce4da535ba72e93867751b4542eea09a
Ajibade Adam <111712386+Adamson123@users.noreply.github.com>
2024-03-13 02:45:09 +0100

    Update date.js

--- scripts/utils/date.js
+++ scripts/utils/date.js
@@ -1,10 +1,20 @@
+function getDayOfYear(date) {
+  var start = new Date(date.getFullYear(), 0, 0);
+  var diff = date - start;
+  var oneDay = 1000 * 60 * 60 * 24;
+  var dayOfYear = Math.floor(diff / oneDay);
+  return dayOfYear;
+}
+
+
+
 function getDate(time) {
   const today = new Date();
 
   if (time === "year") {
     return today.getFullYear().toString().slice(2);
   } else if (time === "day") {
-    return today.getDay();
+    return getDayOfYear(today)
   } else if (time === "hour") {
     return today.getHours();
   } else if (time === "minute") {
