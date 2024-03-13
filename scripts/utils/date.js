50e880ee23f1463cda8e2e103d7ddcfad0cea216
Ajibade Adam <111712386+Adamson123@users.noreply.github.com>
2024-03-13 02:35:50 +0100

    Update date.js

--- scripts/utils/date.js
+++ scripts/utils/date.js
@@ -4,7 +4,7 @@ function getDate(time) {
   if (time === "year") {
     return today.getFullYear().toString().slice(2);
   } else if (time === "day") {
-    return today.getDay();
+    return today.getDays();
   } else if (time === "hour") {
     return today.getHours();
   } else if (time === "minute") {
